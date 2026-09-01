a9978a997d72f1b563b270ec64813d82396e4947
daime <dmytrothedev@gmail.com>
2026-09-01 23:04:41 +0200

    Add basic calculator operations

---
+++ script.js
@@ -0,0 +1,23 @@
+function add (x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+function multiply (x, y){
+    return x * y;
+}
+
+function divide (x, y){
+    return x / y;
+}
+
+console.log(add(5, 2));
+
+console.log(subtract(5,2));
+
+console.log(multiply(5,2));
+
+console.log(divide(5,2));
