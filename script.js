43a6b09371e5be40266713a111fb2cd95c96e9e7
daime <dmytrothedev@gmail.com>
2026-09-01 23:31:23 +0200

    Add operate function

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-function add (x, y){
-    return x + y;
+let num1;
+let num2;
+let operator;
+
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
 }
 
-function subtract(x, y){
-    return x - y;
-}
+function operate(operator, num1, num2){
+    switch (operator){
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    };
+};
 
-function multiply (x, y){
-    return x * y;
-}
-
-function divide (x, y){
-    return x / y;
-}
-
-console.log(add(5, 2));
-
-console.log(subtract(5,2));
-
-console.log(multiply(5,2));
-
-console.log(divide(5,2));
+console.log(operate("+", 1, 6));
+console.log(operate("-", 1, 6));
+console.log(operate("*", 1, 6));
+console.log(operate("/", 1, 6));
